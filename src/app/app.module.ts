7f8114a74f1cf10aca15167f773ff1eff0e8269b
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-09 17:57:11 -0400

    estructura seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NotpagefoundComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
