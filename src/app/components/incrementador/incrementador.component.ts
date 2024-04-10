1a5c6a3f636110dc7cb4c471f574dc2f65363a18
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-10 03:44:46 -0400

    incrementador implementados con Inputs y Outputs como componente reutilizable y aplicados en el dashboard/progress

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,34 +1,51 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: ``
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
 
-  // esperando un input con nombre especial que corresponde a la variable definida
-  @Input('valor') progreso: number = 50;
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`;
+  }
   // default, espera un input de nombre progreso
   // @Input() progreso: number = 50;
+  // esperando un input con nombre especial que corresponde a la variable definida
+  @Input('valor') progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
 
   // @Output() valorSalida: EventEmitter<number> = new EventEmitter();
   @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
-  cambiarValor(valor: number) {
+  cambiarValor( valor: number ) {
 
-    if (this.progreso >= 100 && valor > 0) {
+    if ( this.progreso >= 100 && valor > 0 ) {
       this.valorSalida.emit(100);
       return this.progreso = 100;
     }
-    
-    if (this.progreso <= 0 && valor < 0) {
+
+    if ( this.progreso <= 0 && valor < 0 ) {
       this.valorSalida.emit(0);
       return this.progreso = 0;
     }
-    
+
     this.progreso = this.progreso + valor;
     this.valorSalida.emit( this.progreso );
     return this.progreso;
+  }
+  
+  onChange( nuevoValor : number ){
+    
+    if ( nuevoValor >= 100 ) {
+      this.progreso = 100;
+    } else if ( nuevoValor <= 0 ) {
+      this.progreso = 0;    
+    } else {
+      this.progreso = nuevoValor
+    }
+
+    this.valorSalida.emit( nuevoValor );
   }
 }
