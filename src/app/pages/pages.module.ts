88104fd0d3aacfe4b41cb60477dd42fe578fb019
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-10 03:06:18 -0400

    importando FormsModule y ComponentsModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
+// Módulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+// Componentes
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -24,8 +29,10 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [ 
     CommonModule,
-    SharedModule,
-    RouterModule
+    FormsModule,
+    RouterModule,
+    ComponentsModule,
+    SharedModule
   ]
 })
 
