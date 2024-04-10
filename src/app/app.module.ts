20a266c4178dd06ba36be1d2f10bd5e45ee1d7c5
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-09 23:48:04 -0400

    actualizando importaciones de modulos nuevos para no declarar todo en nuestro app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    RegisterComponent,
-    LoginComponent,
-    DashboardComponent,
-    NotpagefoundComponent,
-    Grafica1Component,
-    ProgressComponent,
-    PagesComponent
+    NotpagefoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
